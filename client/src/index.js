b9628badd2ee4e8ffaf79c620f1cf998f4d578c0
Tucker Steil <tuckersteil@gmail.com>
2023-10-03 13:30:58 -0700

    moving over files

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from "react-router-dom"
+// import ReactDOM  from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+        <App />
+  </BrowserRouter>
 );
+;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
