873de4b5af1a6d6cba825b520349004f9acc6f8d
Tucker Steil <tuckersteil@gmail.com>
2023-10-04 13:07:24 -0700

    fixing icon

--- client/src/SearchBar1.js
+++ client/src/SearchBar1.js
@@ -10,6 +10,7 @@ function SearchBar() {
     <div className="search-bar1">
       <div className="search-input-container1">
         <input type="text" placeholder="Search Marketplace" className="search-input1" />
+        <i class="fas fa-search search-icon1"></i>
         <i className="fas fa-search search-icon1" onClick={handleSearch}></i>
       </div>
     </div>
